dd39625f0c4d03aaba9b44b94dab049aa38c1839
urooj <uroojshaukat6@gmail.com>
2023-07-15 01:14:00 +0500

    model added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const JSONbig = require('json-bigint');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const prisma = new PrismaClient();
+
+router.get('/', async function (req, res, next) {
+  try {
+    const allPricePlots = await prisma.plotPrice.findMany();
+    const jsonSerializer = JSONbig({ storeAsString: true });
+
+    // Serialize the BigInt values using json-bigint
+    const serializedPricePlots = jsonSerializer.stringify(allPricePlots);
+
+    res.send(serializedPricePlots);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
